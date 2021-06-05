78ba1fb1a0738db16ffd5170cf41aaefd12bf3ae
Mericuries <85179622+Mericuries@users.noreply.github.com>
2021-06-05 04:12:51 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+   
+
+
+//  box.onclick = function(event) {
+//     if (event.target.className != 'btn') return;
+
+//      let text = event.target.closest('.text');
+//      text.remove();
+//   };
+
+
+//  let btn = document.querySelector(".btn");
+//  let box = document.getElementById("box");
+
+//  box.addEventListener("click", hidden); 
+//  function hidden(){
+
+//      let text = document.querySelector(".text");
+//      text.hidden = true;
+//      return text;
+//  } 
